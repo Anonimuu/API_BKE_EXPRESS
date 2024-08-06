aefc40f9d2151f0622f5a5776445095fd56c1450
Anonimuu <gkiss7872@gmail.com>
2024-08-06 17:49:18 -0300

    ADD THUNDER

--- src/server.js
+++ src/server.js
@@ -2,14 +2,45 @@ import express from "express"
 const app = express()
 
 
-app.get('/',(req, res ) => {
+app.post('/auth/signup',(req, res ) => {
     res.json({
-        message: "Ola Mundo Express!",
-        seccess: "Tudo certo"
-    
-    
+        message: "Rota de post Auth/Signup",
     })
-
+})
+app.post('/auth/login',(req, res ) => {
+    res.json({
+        message: "Rota de post Auth/Login",
+    })
+})
+app.post('/auth/logout',(req, res ) => {
+    res.json({
+        message: "Rota de post Auth/Logout",
+    })
+})
+app.post('/accout',(req, res ) => {
+    res.json({
+        message: "Rota de post Accout",
+    })
+})
+app.get('/accout',(req, res ) => {
+    res.json({
+        message: "Rota de post Account/List",
+    })
+})
+app.get('/accout',(req, res ) => {
+    res.json({
+        message: "Rota de GET Account ID",
+    })
+})
+app.put('/accout',(req, res ) => {
+    res.json({
+        message: "Rota de PUT Account",
+    })
+})
+app.delete('/accout',(req, res ) => {
+    res.json({
+        message: "Rota de delete Account",
+    })
 })
 
 app.listen(3000,() => {
