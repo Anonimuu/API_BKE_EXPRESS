57b6652bec987e0aeef3eae6a4c08bd7829a51a8
Anonimuu <gkiss7872@gmail.com>
2024-08-06 15:51:27 -0300

    add lib express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log("Ola mundo")
+import express from "express"
+const app = express()
+
+
+app.get('/',(req, res ) => {
+    res.json({
+        message: "Ola Mundo Express!",
+        seccess: "Tudo certo"
+    
+    
+    })
+
+})
+
+app.listen(3000,() => {
+    console.log('servidor on| rodando em http://localhost:3000')
+})
